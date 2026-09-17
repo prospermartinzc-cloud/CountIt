07049f4ae780f14649205881ae291de5764bfe43
prospermartinzc-cloud <prosper.martinzc@gmail.com>
2026-09-17 10:32:07 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,90 @@
+/* =========================
+   COUNTIT APP DATA
+========================= */
+
+// Get Cells
+function getCells() {
+
+  return Number(
+    localStorage.getItem("cells")
+  ) || 0;
+
+}
+
+
+// Get Streak
+function getStreak() {
+
+  return Number(
+    localStorage.getItem("streak")
+  ) || 0;
+
+}
+
+
+// Check if a lesson is completed
+function isLessonCompleted(lessonNumber) {
+
+  return (
+    localStorage.getItem(
+      "lesson" + lessonNumber + "Completed"
+    ) === "true"
+  );
+
+}
+
+
+// Count completed lessons
+function getCompletedLessons() {
+
+  let completed = 0;
+
+  for (
+    let i = 1;
+    i <= 10;
+    i++
+  ) {
+
+    if (
+      isLessonCompleted(i)
+    ) {
+
+      completed++;
+
+    }
+
+  }
+
+  return completed;
+
+}
+/* =========================
+   ADD CELLS
+========================= */
+
+function addCells(amount) {
+
+  let cells = getCells();
+
+  cells += amount;
+
+  localStorage.setItem(
+    "cells",
+    cells
+  );
+
+  return cells;
+
+}
+/* =========================
+   COMPLETE LESSON
+========================= */
+
+function completeLesson(lessonNumber) {
+
+  localStorage.setItem(
+    "lesson" + lessonNumber + "Completed",
+    "true"
+  );
+
+}
