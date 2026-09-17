bbffc2186d0c23c2822daaceb28f0443cbd1e3ac
prospermartinzc-cloud <prosper.martinzc@gmail.com>
2026-09-17 10:31:25 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "countit-v1";
+
+const FILES_TO_CACHE = [
+  "index.html",
+  "style.css",
+  "lessons.html",
+  "achievements.html",
+  "profile.html",
+  "lesson1.html",
+  "lesson2.html",
+  "lesson3.html",
+  "lesson4.html",
+  "lesson5.html",
+  "lesson6.html",
+  "lesson7.html",
+  "lesson8.html",
+  "lesson9.html",
+  "lesson10.html",
+  "manifest.json",
+  "app.js",
+  "icon.png"
+];
+
+
+// INSTALL
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+
+        return cache.addAll(FILES_TO_CACHE);
+
+      })
+
+  );
+
+});
+
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
